d3b1a638a99a51b3d9e7dd20b674cded7810587a
samm81 <samwmaynard@gmail.com>
2026-06-21 17:56:54 +0000

    feat: postgres backups

--- backup/opt/scripts/backup-now.bash
+++ backup/opt/scripts/backup-now.bash
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#STAMP="$(date -u +%Y-%m-%dT%H-%M-%SZ)"
-#OUTDIR="$HOME/opt/backups/postgres"
-#DUMPFILE="$OUTDIR/postgres-all-$STAMP.sql.gz"
-#
-#mkdir -p "$OUTDIR"
-#
-## Dump Postgres from the Docker container
-#docker exec coredb-postgres-1 pg_dumpall -U postgres | gzip -9 > "$DUMPFILE"
-#
-## Keep only 7 days of raw dumps locally
-#find "$OUTDIR" -type f -name 'postgres-all-*.sql.gz' -mtime +7 -delete
+STAMP="$(date -u +%Y-%m-%dT%H-%M-%SZ)"
+OUTDIR="$HOME/opt/backups/postgres"
+DUMPFILE="$OUTDIR/postgres-all-$STAMP.sql.gz"
+
+mkdir -p "$OUTDIR"
+
+# Dump Postgres from the Docker container
+docker exec postgres-postgres-1 pg_dumpall -U postgres | gzip -9 > "$DUMPFILE"
+
+# Keep only 7 days of raw dumps locally
+find "$OUTDIR" -type f -name 'postgres-all-*.sql.gz' -mtime +7 -delete
 
 # Backup to restic
 restic backup \
@@ -22,6 +22,7 @@ restic backup \
   --exclude "${HOME}/opt/**/cache" \
   --exclude "${HOME}/opt/state/beszel" \
   --exclude "${HOME}/opt/state/caddy" \
+  --exclude "${HOME}/opt/state/postgres" \
   "${HOME}/etc" \
   "${HOME}/.local/state" \
   --exclude-caches
